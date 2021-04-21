616ca277f0600b24228a1fc4bac98682d7a68222
Dar Dualba <ddualba@gmail.com>
2021-04-20 17:24:55 -0700

    add formik, refactor add coin form to useFormik

--- client/src/components/coins/CoinForm.js
+++ client/src/components/coins/CoinForm.js
@@ -1,27 +1,27 @@
 import React, { Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
-import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addCoin } from '../../actions/coin';
+import { useFormik } from 'formik';
 
 import './CoinForm.scss';
 
 const CoinForm = ({ addCoin, history }) => {
-  const [formData, setFormData] = useState({
-    symbol: '',
-    name: '',
-    exchange: '',
-    quantity: ''
+  const formik = useFormik({
+    initialValues: {
+      symbol: '',
+      name: '',
+      exchange: '',
+      quantity: ''
+    },
+    onSubmit: values => {
+      handleSubmit(values);
+    }
   });
 
-  const { exchange, symbol, name, quantity } = formData;
-
-  const onChange = e =>
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-
-  const onSubmit = e => {
-    e.preventDefault();
+  const handleSubmit = values => {
+    const { symbol, name, exchange, quantity } = values;
     addCoin({ symbol, name, exchange, quantity, history });
   };
 
@@ -30,7 +30,7 @@ const CoinForm = ({ addCoin, history }) => {
       <div className='coinform-page'>
         <h1 className='coinform__title'>Add coin form</h1>
 
-        <form onSubmit={e => onSubmit(e)} className='coinform'>
+        <form onSubmit={formik.handleSubmit} className='coinform'>
           <div className='coinform__group'>
             <input
               id='symbol'
@@ -39,8 +39,8 @@ const CoinForm = ({ addCoin, history }) => {
               className='coinform__input'
               placeholder='Coin symbol'
               name='symbol'
-              value={symbol}
-              onChange={e => onChange(e)}
+              value={formik.values.symbol}
+              onChange={formik.handleChange}
             />
             <label htmlFor='symbol' className='coinform__label'>
               Coin Symbol
@@ -55,8 +55,8 @@ const CoinForm = ({ addCoin, history }) => {
               className='coinform__input'
               placeholder='Coin name'
               name='name'
-              value={name}
-              onChange={e => onChange(e)}
+              value={formik.values.name}
+              onChange={formik.handleChange}
             />
             <label htmlFor='name' className='coinform__label'>
               Coin name
@@ -71,8 +71,8 @@ const CoinForm = ({ addCoin, history }) => {
               className='coinform__input'
               placeholder='Exchange or wallet name'
               name='exchange'
-              value={exchange}
-              onChange={e => onChange(e)}
+              value={formik.values.exchange}
+              onChange={formik.handleChange}
             />
             <label htmlFor='exchange' className='coinform__label'>
               Exchange/Wallet Name
@@ -88,8 +88,8 @@ const CoinForm = ({ addCoin, history }) => {
               className='coinform__input'
               placeholder='Quantity'
               name='quantity'
-              value={quantity}
-              onChange={e => onChange(e)}
+              value={formik.values.quantity}
+              onChange={formik.handleChange}
             />
             <label htmlFor='quantity' className='coinform__label'>
               Quantity
